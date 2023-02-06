6fcf2c64cb6a303b5964576189a78db87b8472fe
yovi18 <yovi18azhari@gmail.com>
2023-02-06 16:26:41 +0700

    tambah konten

--- Template/admin 2/scripts.js
+++ Template/admin 2/scripts.js
@@ -3,10 +3,12 @@ const body = document.querySelector("body"),
       toggle = body.querySelector(".toggle"),
       searchBtn = body.querySelector(".search-box"),
       modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
+      modeText = body.querySelector(".mode-text"),
+      conten = body.querySelector(".conten");
 
 toggle.addEventListener("click",() => {
-  sidebar.classList.toggle("close")
+  sidebar.classList.toggle("close"),
+  conten.classList.toggle("tutup")
 });
 
 modeSwitch.addEventListener("click",() => {
